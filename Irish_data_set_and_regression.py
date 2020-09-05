8f4e0267831a0b65dac83ecf7eefd15ca77a2709
minnu351 <69900550+minnu351@users.noreply.github.com>
2020-09-05 10:19:40 +0400

    Update Irish_data_set_and_regression.py

--- Irish_data_set_and_regression.py
+++ Irish_data_set_and_regression.py
@@ -5,11 +5,8 @@ from sklearn.datasets import load_iris
 
 iris = load_iris() #['sepal length (cm)','sepal width (cm)','petal length (cm)','petal width (cm)'],
 
-
 x = iris.data         # iris['data']
 y = iris.target       # iris['target']
-
-
 
 print(iris['target_names'])
 
@@ -18,7 +15,6 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=0)
 Lin = LinearRegression()
 
 Lin.fit(x_train,y_train)
-
 
 print(Lin.coef_) # Here we find b1 of equation b0+b1*x
 
@@ -33,7 +29,6 @@ print(acc)
 #The Mean Absolute Error(MAE) is the average of all absolute errors.
 print(mean_absolute_error(y_test,Pred_y))
 
-
-#RMSE is a quadratic scoring rule that also measures the average magnitude of the error.
+#RMSE is a quadratic scoring rule - also measures the average magnitude of the error.
 # The average squared difference between the estimated values and the actual value
 print(mean_squared_error(y_test,Pred_y))
